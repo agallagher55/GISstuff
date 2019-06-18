49be3d6c5cdac350e75094725e6291937678ee5b
Alex <agallagher55@gmail.com>
2019-06-18 11:28:43 -0300

    case statement added to hangman to implement difficulty

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -1,30 +1,37 @@
+# display some sort of count so the player knows how many more incorrect guesses he/she has before the game ends. 
+# You should also display which correct letters have already been chosen (and their position in the word, e.g. _ r o g r a _ _ i n g) and 
+# which incorrect letters have already been chosen.
+
+# June 18, 2019
+# Alex Gallagher
+
+# Add function to guess word
 
 puts "Initializing!"
 filename = "5desk.txt"
 
 # Randomly select a word between 5 and 12 characters long for the secret word.
-def rand_line(file, len1, len2)
+def rand_line(file, min, max)
     words = File.readlines(file)
     suitable = Array.new()
     
     words.each do |word|
-        if word.length > len1 && word.length < len2
+        if word.length > min && word.length < max
             suitable.push(word)
         end
     end
-
     rand_word = suitable[rand(0..suitable.length)]
 end
 
-secret_word = rand_line(filename, 3, 6).upcase.chomp
+puts "Shortest Word Length: "
+min = gets.chomp.to_i
+puts "Longest Word Length: "
+max = gets.chomp.to_i
+
+secret_word = rand_line(filename, min, max).upcase.chomp
 puts "\nSECRET WORD: #{secret_word}\n"
 
-# display some sort of count so the player knows how many more incorrect guesses he/she has before the game ends. 
-# You should also display which correct letters have already been chosen (and their position in the word, e.g. _ r o g r a _ _ i n g) and 
-# which incorrect letters have already been chosen.
-
 # Global Variables
-guesses = 5
 number_right = 0
 
 guessed = Array.new()
@@ -34,11 +41,22 @@ hidden_word = "_"*secret_word.length
 win = false
 
 uniq_chars = secret_word.chars.uniq
-# uniq_chars.pop
 
-puts "Unique Chars: #{uniq_chars}"
-puts "Number of Unique Characters: #{uniq_chars.length}"
-puts "Secret word has #{secret_word.length} letters"
+# Difficulty
+puts "\nPlease choose your difficulty: Easy, Medium, Hard, Expert"
+answer = gets.chomp.upcase
+case answer
+when "EASY"
+    guesses = secret_word.length + 8
+when "MEDIUM"
+    guesses = secret_word.length + 6
+when "HARD"
+    guesses = secret_word.length + 4
+when "EXPERT"
+    guesses = secret_word.length + 2
+end
+
+puts "Secret word has #{secret_word.length} letters (#{uniq_chars.length} unique)"
 
 while guesses > 0 && win == false
     puts "\nSECRET WORD: #{hidden_word}"
@@ -47,7 +65,7 @@ while guesses > 0 && win == false
     puts "\tGuesses Remaining: #{guesses}"
 
     puts "\nGuess a letter: "
-    guessed_letter = gets.chomp.upcase
+    guessed_letter = gets.strip().upcase
     
     # Check if guess is in word
     if guessed.include?(guessed_letter) == false 

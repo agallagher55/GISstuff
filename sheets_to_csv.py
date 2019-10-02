88f5d2a19a6a317738568490c0af2e8a66620035
agallagher55 <37911372+agallagher55@users.noreply.github.com>
2019-10-02 09:37:49 -0300

    Update sheets_to_csv.py

--- sheets_to_csv.py
+++ sheets_to_csv.py
@@ -1,3 +1,22 @@
+def sheets_tocsvs():
+    import os, sys
+    import pandas as pd
+    # Can hardcode your filename to variable below or input data from prompt provided
+    # target_xlsx = r'kijiji_data.xls'
+    target_xlsx = raw_input("Please type name of excel file, including extension: ")
+
+    xl_file = pd.ExcelFile(target_xlsx)
+
+    sheet_names = xl_file.sheet_names
+    sheet_data = [xl_file.parse(sheet) for sheet in sheet_names]
+
+    sheet_dict = dict(zip(sheet_names, sheet_data))
+
+    # Write sheets to own .csv
+    for name, data in sheet_dict.items():
+        print "Writing '{}' to .csv".format(name)
+        data.to_csv(name + '_' + os.path.basename(target_xlsx).split('.')[0] + '.csv', encoding='utf-8', index=False)
+
 def merge_csvs(filextension, filename='output'):
     import os, sys
     import pandas as pd
